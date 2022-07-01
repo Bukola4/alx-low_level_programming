d67c502775903df3e110e6ae16cb46ed3401d755
Bukola4 <bukky.ruq@gmail.com>
2022-07-01 13:11:08 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include <stdlib.hii>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
@@ -13,7 +13,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
