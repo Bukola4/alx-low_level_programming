94f686148110379bdf9b5bc5432726371d7d3650
Bukola4 <bukky.ruq@gmail.com>
2022-07-04 12:55:28 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,23 +7,34 @@
  */
 int main(void)
 {
-	int d, p, q;
+	int c;
+	int d;
+	int e = 0;
 
-	for (d = 0; d < 8; d++)
+	while (e < 10)
 	{
-	for (p = d + 1; p  < 9; p++)
+		d = 0;
+	while (d < 10)
 	{
-	for (q = p + 1; q  < 10; q++)
+		c = 0;
+	while (c < 10)
 	{
-	putchar((d % 10) + '0');
-	putchar((p % 10) + '0');
-	putchar((q % 10) + '0');
-	if (d == '7' && p == '8' && q == '9')
-	continue;
+	if (c != d && d != e && e < d && d < c)
+	{
+	putchar('0' + e);
+	putchar('0' + d);
+	putchar('0' + c);
+	if (c + d + e != 9 + 8 + 7)
+	{
 	putchar(',');
 	putchar(' ');
 	}
 	}
+	c++;
+	}
+	d++;
+	}
+	e++;
 	}
 	putchar('\n');
 	return (0);
