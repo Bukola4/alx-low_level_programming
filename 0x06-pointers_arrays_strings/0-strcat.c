f9bd879f8757762ebf233dc4bf8a3bb23db37341
Bukola4 <bukky.ruq@gmail.com>
2022-07-14 12:09:30 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,7 @@
 * @src: second param
 * Return: a string
 */
-char *_strcat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 int len = 0, i;
 
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,9 +5,9 @@
 * @x: param x
 * Return: a string
 */
-char *leet(char *)
+char *leet(char *x)
 {
-int a = 0, b, 1 = 5;
+int a = 0, b, l = 5;
 char tr[5] = {'A', 'E', 'O', 'T', 'L'};
 char trw[5] = {'4', '3', '0', '7', '1'};
 
@@ -15,7 +15,7 @@ while (x[a])
 {
 b = 0;
 
-while (b < 1)
+while (b < l)
 {
 if (x[a] == tr[b] || x[a] - 32 == tr[b])
 x[a] = trw[b];
