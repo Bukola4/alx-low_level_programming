86a6020f4667ca806ec81fd86c8c08a393439a8b
Bukola4 <bukky.ruq@gmail.com>
2022-07-18 09:16:00 -0700

    101-crackme_password

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+/*
+* File: 9-set_string.c
+* Auth: Bukola Adegboyega
+*/
+
+#include "main.h"
+
+/**
+* set_string - sets the value of a pointer to a char
+* @s: the pointer
+* @to: the char
+*/
+void set_string(char **s, char *to)
+{
+*s = t0;
+}
