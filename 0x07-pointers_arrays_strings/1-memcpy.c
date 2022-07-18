073b4f87fc20edde4947bc2f003cc9fdd3143ace
Bukola4 <bukky.ruq@gmail.com>
2022-07-18 10:34:46 -0700

    task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,28 +1,17 @@
-/*
-* File: 1-memcpy.c
-* Auth: Bukola Adegboyega
-*/
-
 #include "main.h"
+
 /**
 * _memcpy - memory copy
 * @dest: destination memory
 * @src: source memory
 * @n: number of bytes to be copied
-*
 * Return: string copied from source
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i = 0;
-int j = 0;
+unsigned int a;
 
-while (n > 0)
-{
-dest[i] = src[j];
-i++;
-j++;
-n--;
-}
+for (a = 0; a < n; a++)
+dest[a] = src[a];
 return (dest);
 }
