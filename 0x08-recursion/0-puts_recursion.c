9cadabc09196ee31d9727c1eb5a9f28fab63fef8
Bukola4 <bukky.ruq@gmail.com>
2022-07-20 07:18:01 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,17 @@
 #include "main.h"
+
 /**
-* _puts_recursion - prints a string
-* @s: pointer block of memory to fill
-* Return: void
+* _puts_recursion - function to  print string
+* @s: string parameter
 */
-
 void _puts_recursion(char *s)
-
 {
 if (*s == '\0')
 {
 _putchar('\n');
 return;
 }
-
 _putchar (*s);
-_puts_recursion(s + 1);
+s++;
+_puts_recursion(s);
 }
